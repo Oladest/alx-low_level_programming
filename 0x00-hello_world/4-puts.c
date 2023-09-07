4dd6f03d84496ad256167f134e9322f2fac748d9
Oladest <oladiransola5@gmail.com>
2023-09-07 10:47:48 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - The program that prints puts function
+  *return: 0 (success)
+ */
+ int main(void)
+{
+       puts("\"programming is like building a multilingual puzzle");   
+       return (0);
+}
