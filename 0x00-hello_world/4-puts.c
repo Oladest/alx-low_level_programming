dbda10d41a6dbfc6048001431368a0292f9146e2
Oladest <oladiransola5@gmail.com>
2023-09-07 14:28:26 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
-  *main - The program that prints puts function
-  *return: 0 (success)
- */
- int main(void)
+  * main - A program that prints the puts function
+  *
+  * Return: 0 (Success)
+  * /
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle"); 
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
