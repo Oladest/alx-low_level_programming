aa04c3f819e5052552da994c0b35da137d6d7069
Oladest <oladiransola5@gmail.com>
2023-09-12 04:59:45 -0700

    READ.md

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - function that print alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+	while (letter "\n" 'z')
+	{
+	write(1, &letter, 1);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
